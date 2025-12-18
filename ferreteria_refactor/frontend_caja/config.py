a2446325644e2033ebcd0ac92d7b263d30b781f2
Gabo <rodriguezisaac876@gmail.com>
2025-12-18 11:03:35 -0400

    limpieza de proyecto

--- ferreteria_refactor/frontend_caja/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load .env explicitly from current directory or executable directory
-load_dotenv()
-
-API_BASE_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
